9e0e0c5d4328dcead51a5c44355be0e81571c41e
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-03-04 20:00:38 +0500

    add signinWithGoogle

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import {
+    getAuth,
+    RecaptchaVerifier ,
+    signInWithPhoneNumber 
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+export {
+    getAuth,
+    RecaptchaVerifier,
+    signInWithPhoneNumber 
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
